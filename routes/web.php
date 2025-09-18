3ca7058665660d53d76917f3703cc1dd1bedc379
sahal muhammed <sahalmhd25@gmail.com>
2025-09-18 17:46:48 +0000

    new

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\InvoiceSystem\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return redirect('login');
-// });
-
-Route::get('/', function(){
-
-    return view('welcome');
+Route::get('/', function () {
+    return redirect('login');
 });
+
+// Route::get('/', function(){
+
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
